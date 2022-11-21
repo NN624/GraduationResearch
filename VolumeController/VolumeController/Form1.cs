b7d5f1c52630d6be9b5ecb207e07a95dc276f620
N624 <shiotanagisa18@gmail.com>
2022-11-22 01:02:23 +0900

    update 音量の指定をトラックバーに変更、音量をリアルタイム視覚化

--- VolumeController/VolumeController/Form1.cs
+++ VolumeController/VolumeController/Form1.cs
@@ -8,29 +8,57 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using NAudio.CoreAudioApi;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace VolumeController
 {
     public partial class Form1 : Form
     {
+
+        private VolumeController vc = new VolumeController();
         public Form1()
         {
             InitializeComponent();
-            VolumeController vc = new VolumeController();
+            // 現在の音量を取得して、初期値として設定
             label2.Text = System.Convert.ToString(vc.GetVolume());
+            trackBar1.Value = vc.GetVolume();
+            // デバイスの一覧を取得
+            comboBox1.Items.AddRange(vc.GetDevices());
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            VolumeController vc = new VolumeController();
-            label2.Text = System.Convert.ToString(vc.SetVolume(textBox1.Text));
+            if (comboBox1.SelectedItem != null)
+            {
+                // パーの値を変更
+                var device = (MMDevice)comboBox1.SelectedItem;
+                progressBar1.Value = (int)(Math.Round(device.AudioMeterInformation.MasterPeakValue * 100));
+                // Debug.WriteLine(device.AudioMeterInformation.MasterPeakValue);
+                // Debug.WriteLine(progressBar1.Value);
+
+                // 音量を取得
+                var volume = vc.GetVolume();
+                Debug.WriteLine(progressBar1.Value * volume);
+
+                // 音量を制限
+                if (progressBar1.Value * volume > 2000)
+                {
+                    label2.Text = System.Convert.ToString(vc.SetVolume(volume - 10));
+                    trackBar1.Value = volume;
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            vc.SetVolume(trackBar1.Value);
+            label2.Text = System.Convert.ToString(vc.SetVolume(trackBar1.Value));
         }
     }
 
     // 音量調整をする
     class VolumeController
     {
-
         public MMDevice device;
 
         // VolumeControllerのコンストラクター
@@ -40,37 +68,26 @@ namespace VolumeController
             device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
         }
 
-
-        // 音量の変更
-        public int SetVolume(string value)
+        // メソッド:音量の変更
+        public int SetVolume(int volume)
         {
-            int volume = 0;
-            // strからintへ変換
-            try
-            {
-                volume = System.Convert.ToInt32(value);
-            }
-            catch (System.OverflowException)
-            {
-                Debug.WriteLine("Overflow in double to int conversion.");
-            }
-            // 音量の範囲の判定
-            if (0 <= volume && volume <= 100)
-            {
-                device.AudioEndpointVolume.MasterVolumeLevelScalar = ((float)volume / 100.0f);
-                
-            }
-            else
-            {
-                Debug.WriteLine("Volume is over range.");
-                volume = (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
-            }
-            return volume;
+            // 音量を変更（範囲：0.0～1.0）
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = ((float)volume / 100.0f);
+            return GetVolume();
         }
-        //音量の取得
+
+        // メソッド：音量の取得
         public int GetVolume()
         {
             return (int)(device.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+        }
+
+        // メソッド：デバイスの一覧を取得
+        public object[] GetDevices()
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            var devices = enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+            return devices.ToArray();
         }
     }
 }
